0f4bcae55563abb565af35b999fe50e3c3aa8f6b
Cauterization <71918928+Cauterization@users.noreply.github.com>
2022-11-29 22:12:24 +0300

    Move mkRefs from encoins tests

--- src/Test/TestingServer.hs
+++ src/Test/TestingServer.hs
@@ -1,11 +1,19 @@
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 
 module Test.TestingServer where
 
-import PlutusTx.Builtins.Class (stringToBuiltinByteString)
-import Server.Internal         (HasServer(processTokens))
-import Test.Internal           (runTestM, testBalance, testBalanceAll)
-import TestingServer.Main      (TestingServer)
+import           Control.Monad.IO.Class  (MonadIO(..))
+import qualified Data.Map                as Map
+import           IO.ChainIndex           (getUtxosAt)
+import           IO.Wallet               (getWalletAddr)
+import           PlutusTx.Builtins.Class (stringToBuiltinByteString)
+import           Server.Internal         (HasServer(..))
+import           Server.Tx               (mkWalletTxOutRefs) 
+import           Test.Internal           (runTestM, testBalance, testBalanceAll)
+import           TestingServer.Main      (TestingServer)
+import           Utils.ChainIndex        (filterCleanUtxos)
+import           Utils.Logger            (HasLogger(..), logSmth, (.<))
 
 testBalanceTS :: IO ()
 testBalanceTS = testBalance @TestingServer
@@ -15,3 +23,15 @@ testBalanceAllTS = testBalanceAll @TestingServer
 
 testMintTS :: [String] -> IO ()
 testMintTS = runTestM @TestingServer . processTokens . map stringToBuiltinByteString
+
+mkRefs :: Int -> IO ()
+mkRefs n = runTestM @TestingServer $ do
+    addr  <- getWalletAddr
+    utxos <- liftIO (getUtxosAt addr)
+    refs   <- mkWalletTxOutRefs addr n
+    utxos' <- liftIO $ getUtxosAt addr
+    logMsg $ "Past utxo's ammount: "          .< Map.size utxos
+    logMsg $ "Past clean utxo's ammount: "    .< Map.size (filterCleanUtxos utxos)
+    logMsg $ "Current utxo's ammount: "       .< Map.size utxos'
+    logMsg $ "Current clean utxo's ammount: " .< Map.size (filterCleanUtxos utxos')
+    logSmth refs
