f2bb395d95673d4b9d7cbf1104dff22ca96aacbf
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-01-09 16:25:51 +0300

    Change ClientM to AppM

--- src/Client/Class.hs
+++ src/Client/Class.hs
@@ -1,36 +1,15 @@
 {-# LANGUAGE AllowAmbiguousTypes        #-}
 {-# LANGUAGE DerivingStrategies         #-}
 {-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE TypeFamilies               #-}
 
 module Client.Class where
 
 import           Data.Kind            (Type)
-import           Control.Monad.Reader (asks, MonadIO, MonadReader, ReaderT(..))
-import           IO.Wallet            (HasWallet(..), RestoredWallet)
 import           Options.Applicative  (Parser)
-import qualified Server.Class         as Server
-import           Utils.Logger         (HasLogger(..))
+import           Server.Class         (HasServer(..), AppM)
 
-newtype ClientM s a = ClientM { unClientM :: ReaderT (Env s) IO a }
-    deriving newtype (Functor, Applicative, Monad, MonadReader (Env s), MonadIO)
-
-runClientM :: Server.AuxiliaryEnvOf s -> RestoredWallet -> ClientM s a -> IO a
-runClientM aEnv wallet = flip runReaderT (Env aEnv wallet) . unClientM
-
-data Env s = Env
-    { envAuxiliary :: Server.AuxiliaryEnvOf s
-    , envWallet    :: RestoredWallet
-    }
-
-instance HasLogger (ClientM s) where
-    loggerFilePath = "client.log"
-
-instance HasWallet (ClientM s) where
-    getRestoredWallet = asks envWallet
-
-class ( Server.HasServer c
+class ( HasServer c
       , Show (RequestTermOf c)
       , Eq (RequestTermOf c)
       ) => HasClient c where
@@ -43,6 +22,6 @@ class ( Server.HasServer c
 
     -- here ClientM c () are some additional actions, that would be executed
     -- on successful response
-    makeServerInput :: ClientRequestOf c -> ClientM c (ClientM c (), Server.InputOf c)
+    makeServerInput :: ClientRequestOf c -> AppM c (AppM c (), InputOf c)
 
 type ClientRequestOf s = [RequestTermOf s]
--- src/Client/Main.hs
+++ src/Client/Main.hs
@@ -7,7 +7,7 @@
 
 module Client.Main where
 
-import           Client.Class              (HasClient(..), ClientM, ClientRequestOf, runClientM)
+import           Client.Class              (HasClient(..), ClientRequestOf)
 import           Client.Opts               (AutoOptions(..), Maximum, Options(..), runWithOpts)
 import           Control.Monad             (replicateM)
 import           Control.Monad.Reader      (MonadIO(..), forever, when)
@@ -18,8 +18,8 @@ import           Network.HTTP.Client       (httpLbs, defaultManagerSettings, new
                                             Manager, Request(..), RequestBody(..), responseStatus, responseTimeoutMicro)
 import           Network.HTTP.Types.Header (hContentType)
 import           Network.HTTP.Types.Status (status204)
+import           Server.Class              (AppM, runAppM)
 import           Server.Config             (Config(..), loadConfig)       
-import qualified Server.Class              as Server
 import           System.Random             (randomRIO)
 import           Utils.Logger              (HasLogger(..), (.<))
 import           Utils.Wait                (waitTime)
@@ -28,14 +28,13 @@ startClient :: forall s. HasClient s => IO ()
 startClient = do
     opts <- runWithOpts @s
     Config{..} <- loadConfig 
-    Server.Env{..} <- Server.loadEnv @s
     let fullAddress = "http://"
                    <> T.unpack cServerAddress
                    <> "/relayRequestSubmitTx"
     nakedRequest <- parseRequest fullAddress
     manager <- newManager defaultManagerSettings
     let mkRequest' = mkRequest @s nakedRequest manager
-    runClientM envAuxiliary envWallet $ withGreetings $ case opts of
+    runAppM $ withGreetings $ case opts of
         Manual cReq          -> mkRequest' cReq
         Auto AutoOptions{..} -> forever $ do
             cReq <- genRequest @s maxTokensInReq
@@ -44,7 +43,7 @@ startClient = do
     where
         withGreetings = (logMsg "Starting client..." >>)
 
-mkRequest :: forall s. HasClient s => Request -> Manager -> ClientRequestOf s -> ClientM s ()
+mkRequest :: forall s. HasClient s => Request -> Manager -> ClientRequestOf s -> AppM s ()
 mkRequest nakedReq manager clientReq = do
         logMsg $ "New tokens to send:\n" .< clientReq
         (onSuccess, red) <- makeServerInput clientReq
