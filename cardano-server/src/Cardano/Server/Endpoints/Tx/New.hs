f335fc0a3667d9f1b0dbb44550a499715851885d
encoins <111982001+encryptedcoins@users.noreply.github.com>
2023-03-21 13:42:15 +0100

    Add newTx endpoint signature
    Now also return TxId

--- cardano-server/src/Cardano/Server/Endpoints/Tx/New.hs
+++ cardano-server/src/Cardano/Server/Endpoints/Tx/New.hs
@@ -22,17 +22,19 @@ import           Control.Monad.Catch               (Exception, MonadThrow (throw
 import           Data.Aeson                        (ToJSON)
 import           Data.Text                         (Text)
 import           GHC.Generics                      (Generic)
-import           Ledger                            (CardanoTx)
+import           Ledger                            (CardanoTx, getCardanoTxId, TxId (..))
 import           PlutusAppsExtra.Types.Error       (MkTxError)
 import           PlutusAppsExtra.Utils.Tx          (cardanoTxToText)
+import           PlutusTx.Prelude                  (fromBuiltin)
 import           Servant                           (JSON, Post, ReqBody, (:>))
+import           Text.Hex                          (encodeHex)
 
 type NewTxApi s = "newTx"
               :> Throws NewTxApiError
               :> Throws ConnectionError
               :> Throws MkTxError
               :> ReqBody '[JSON] (TxApiRequestOf s)
-              :> Post '[JSON] Text
+              :> Post '[JSON] (Text, Text)
 
 newtype NewTxApiError = UnserialisableCardanoTx CardanoTx
     deriving (Show, Generic, ToJSON)
@@ -42,14 +44,16 @@ instance IsCardanoServerError NewTxApiError where
     errStatus _ = toEnum 422
     errMsg (UnserialisableCardanoTx tx) = "Cannot serialise balanced tx:" .< tx
 
-newTxHandler :: forall s. HasTxEndpoints s 
+newTxHandler :: forall s. HasTxEndpoints s
     => TxApiRequestOf s
-    -> NetworkM s (Envelope '[NewTxApiError, ConnectionError, MkTxError] Text)
+    -> NetworkM s (Envelope '[NewTxApiError, ConnectionError, MkTxError] (Text, Text))
 newTxHandler req = toEnvelope $ do
     logMsg $ "New newTx request received:\n" .< req
     checkEndpointAvailability isInactiveNewTx
     (input, context) <- txEndpointsProcessRequest req
     balancedTx <- join $ liftM3 mkBalanceTx (serverTrackedAddresses @s) (pure context) (txEndpointsTxBuilders @s input)
     case cardanoTxToText balancedTx of
-        Just res -> pure res
+        Just res ->
+            let txId = encodeHex $ fromBuiltin $ getTxId $ getCardanoTxId balancedTx
+            in pure (txId, res)
         Nothing  -> throwM $ UnserialisableCardanoTx balancedTx
