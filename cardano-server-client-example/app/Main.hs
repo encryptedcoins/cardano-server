4dba716a3252f236f683b90836bd6fb098dac156
Cauterization <71918928+Cauterization@users.noreply.github.com>
2023-08-30 14:25:40 +0300

    Fix config paths in executables

--- cardano-server-client-example/app/Main.hs
+++ cardano-server-client-example/app/Main.hs
@@ -3,4 +3,4 @@ module Main where
 import Cardano.Server.Client.Example.Main (runExampleClient)
 
 main :: IO ()
-main = runExampleClient "cardano-server-test/test/configuration/config.json"
+main = runExampleClient "config.json"
