e621879d5069ed0be030a8397162b2e9569a2c9a
Cauterization <71918928+Cauterization@users.noreply.github.com>
2024-03-29 20:04:52 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cabal new-build all
 cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/cardano-server-example-1.0.0.0/x/cardano-server-example/build/cardano-server-example/cardano-server-example ~/.local/bin/cardano-server-example
-cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/cardano-server-client-example-1.0.0.0/x/cardano-server-client-example/build/cardano-server-client-example/cardano-server-client-example ~/.local/bin/cardano-client-example
+cp dist-newstyle/build/x86_64-linux/ghc-8.10.7/cardano-server-client-example-1.0.0.0/x/cardano-server-client-example/build/cardano-server-client-example/cardano-server-client-example ~/.local/bin/cardano-server-client-example
